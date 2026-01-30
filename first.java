7042b5822f987f89ef9b32a3ae49732d9b70db05
ankitha-nakirakanti30 <ankithanakirakanti@gmail.com>
2026-01-30 10:46:34 +0530

    Refactor first.java to correct class name and output

--- first.java
+++ first.java
@@ -1,7 +1,5 @@
-class first
-{
-public static void main(String args[])
-{
-System.out.println("Hello!World");
-}
+class First {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
